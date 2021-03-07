219f325a3f8c02ecdda5169d495c2760108b1c82
sharkygg <55534910+sharkygg@users.noreply.github.com>
2021-03-07 17:31:00 +0800

    build new contracts;

--- build.sh
+++ build.sh
@@ -23,9 +23,9 @@ function build_module() {
       exit 1
     fi
     echo "copy to ../release"
-    cp ${m_dir}/target/${m_name}.wasm ../release/${m_name}_v$VERSION.wasm
-    cp ${m_dir}/target/${m_name}.contract ../release/${m_name}_v$VERSION.contract
-    cp ${m_dir}/target/metadata.json ../release/${m_name}_v$VERSION.json
+    cp ${m_dir}/target/ink/${m_name}.wasm ../release/${m_name}_v$VERSION.wasm
+    cp ${m_dir}/target/ink/${m_name}.contract ../release/${m_name}_v$VERSION.contract
+    cp ${m_dir}/target/ink/metadata.json ../release/${m_name}_v$VERSION.json
     cd -
 }
 
