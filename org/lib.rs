6e6a0b28299827226cae80f4d909e82b16b1a12d
wxstinexx <77033615+wxstinexx@users.noreply.github.com>
2021-06-16 19:21:27 +0800

    feat(wxstinexx): org contract update for v3.0

--- org/lib.rs
+++ org/lib.rs
@@ -249,7 +249,7 @@ mod org {
         #[ink(message)]
         pub fn batch_add_dao_member(&mut self, members:BTreeMap<String, AccountId>) -> bool {
             for (name, accountId) in &members {
-                self.add_dao_member(name,accountId);
+                self.add_dao_member(*name,*accountId);
             }
             true
             
