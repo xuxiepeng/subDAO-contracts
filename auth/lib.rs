eede79f5a76836a430b33ad7db7333e9531e0a83
mopemope-LeaF <ulaqingdan@gmail.com>
2021-07-15 10:56:04 +0800

    add auth permission

--- auth/lib.rs
+++ auth/lib.rs
@@ -65,6 +65,10 @@ mod auth {
         
         #[ink(message)]
         pub fn has_permission(& self, account_id: AccountId, contract_name: String, function_name: String)  -> bool {
+            return self._has_permission(account_id, contract_name, function_name);
+        }
+
+        fn _has_permission(& self, account_id: AccountId, contract_name: String, function_name: String)  -> bool {
             if let Some(action) = self.actions.get(&(contract_name, function_name)) {
                 if let Some(_) = self.actions_auths.get(&(account_id, action.action_id)) {
                     return true;
@@ -75,7 +79,8 @@ mod auth {
 
         #[ink(message)]
         pub fn grant_permission(& mut self, account_id: AccountId, contract_name: String, function_name: String) ->  bool {
-            assert!(self.owner == self.env().caller());
+            let caller = self.env().caller();
+            assert!(self.owner == caller || self._has_permission(caller, String::from("auth"),String::from("grant")));
             if let Some(action) = self.actions.get(&(contract_name, function_name)){
                 let a: Action = Action{
                     action_id: action.action_id,
@@ -102,7 +107,8 @@ mod auth {
 
         #[ink(message)]
         pub fn revoke_permission(& mut self,account_id: AccountId,contract_name: String, function_name: String) -> bool {
-            assert!(self.owner == self.env().caller());
+            let caller = self.env().caller();
+            assert!(self.owner == caller || self._has_permission(caller, String::from("auth"),String::from("grant")));
             if let Some(action) = self.actions.get(&(contract_name, function_name)){
                 self.actions_auths.take(&(account_id, action.action_id));
                 return true;
@@ -113,7 +119,8 @@ mod auth {
         
         #[ink(message)]
         pub fn register_action(& mut self,contract_name: String, function_name: String, action_title: String) -> bool {
-            assert!(self.owner == self.env().caller());
+            let caller = self.env().caller();
+            assert!(self.owner == caller || self._has_permission(caller, String::from("auth"),String::from("register")));
             let action_id = self.action_id;
             self.action_id += 1;
             let action = Action{
@@ -129,7 +136,8 @@ mod auth {
 
         #[ink(message)]
         pub fn cancel_action(& mut self,contract_name: String, function_name: String) -> bool {
-            assert!(self.owner == self.env().caller());
+            let caller = self.env().caller();
+            assert!(self.owner == caller || self._has_permission(caller, String::from("auth"),String::from("register")));
             self.actions.take(&(contract_name, function_name));
             true
         }
@@ -206,6 +214,21 @@ mod auth {
             match r {
                 true => ink_env::debug_println("grant success"),
                 false => ink_env::debug_println("grant failed"),
+            }
+        }
+
+        #[ink::test]
+        fn test_grant_permission2() {
+            let accounts =ink_env::test::default_accounts::<ink_env::DefaultEnvironment>().expect("Cannot get accounts");
+            let mut auth = Auth::new(accounts.alice);
+            auth.register_action("auth".to_string(), "grant".to_string(), "grant".to_string());
+            auth.register_action("hello".to_string(), "world".to_string(), "access".to_string());
+            auth.grant_permission(accounts.alice, "auth".to_string(), "grant".to_string());
+            auth.transfer_owner(accounts.bob);
+            let r = auth.grant_permission(accounts.bob, "hello".to_string(), "world".to_string());
+            match r {
+                true => ink_env::debug_println("grant2 success"),
+                false => ink_env::debug_println("grant2 failed"),
             }
         }
 
