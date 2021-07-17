a19d13a67559bc8449ec2b2fef5bb7d7705898de
wxstinexx <77033615+wxstinexx@users.noreply.github.com>
2021-07-17 16:30:02 +0800

    Update lib.rs

--- dao_manager/lib.rs
+++ dao_manager/lib.rs
@@ -343,6 +343,7 @@ mod dao_manager {
 
             self.components.org = Some(org_instance);
             self.component_addrs.org_addr = Some(org_addr);
+            let mut auth_instance: Auth = ink_env::call::FromAccountId::from_account_id(auth_addr);
             auth_instance.grant_permission(org_addr, String::from("auth"), String::from("grant"));
             true
         }
